6fb8add9e6f877d3b7b143b36d110b53a3d38b32
David Wilde <d.wilde@selligence.com>
2025-01-09 20:17:13 +0000

    Update nvim config

--- nvim/lua/dwilde/autocmds.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    pattern = { *.ts, *.tsx, *.js, *.jsx },
-    desc = "Prettier on save",
-    callback = function()
-        vim.cmd("FormatWrite")
-    end,
-    group = autocmd_group,
-})
-
--- nvim/lua/dwilde/init.lua
+++
@@ -1,8 +0,0 @@
-require("dwilde.remap")
-require("dwilde.set")
-require("dwilde.packer")
-require("dwilde.dap")
-
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
-vim.g.netrw_browse_split = 0
